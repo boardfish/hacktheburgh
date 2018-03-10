e53bd44568d27c0faaae7e162741db0dda4d287e
Hammie <hcsams1@sheffield.ac.uk>
2018-03-10 15:03:31 +0000

    Added mouse movement

--- src/states/play.js
+++ src/states/play.js
@@ -1,6 +1,8 @@
-var TemplateGame = TemplateGame || {}
+var TemplateGame = TemplateGame || {} 
 
-TemplateGame.Play = new Kiwi.State('Play')
+//var mouse = this.game.input.mouse
+
+TemplateGame.Play = new Kiwi.State( "Play" )
 
 /**
 * The PlayState in the core state that is used in the game.
@@ -8,50 +10,65 @@ TemplateGame.Play = new Kiwi.State('Play')
 * It is the state where majority of the functionality occurs 'in-game' occurs.
 */
 
+
 /**
 * This create method is executed when a Kiwi state has finished loading
 * any resources that were required to load.
 */
+
+TemplateGame.create = function () {
+
+	this.setupUpMouse( 15, 15 )
+
+
+}
 TemplateGame.Play.create = function () {
-  Kiwi.State.prototype.create.call(this)
 
-  /*
-  * Replace with your own game creation code here...
-  */
-  this.name = new Kiwi.GameObjects.StaticImage(
-    this, this.textures.kiwiName, 10, 10)
+	Kiwi.State.prototype.create.call( this )
 
-  this.heart = new Kiwi.GameObjects.Sprite(
-    this, this.textures.icons, 10, 10)
-  this.heart.cellIndex = 8
-  this.heart.y = this.game.stage.height - this.heart.height - 10
+	/*
+	* Replace with your own game creation code here...
+	*/
+	this.name = new Kiwi.GameObjects.StaticImage(
+		this, this.textures.kiwiName, 10, 10)
 
-  this.shield = new Kiwi.GameObjects.Sprite(
-    this, this.textures.icons, 200, 200)
-  this.shield.cellIndex = 9
-  this.shield.y = this.game.stage.height * 0.5 - this.shield.height * 0.5
-  this.shield.x = this.game.stage.width * 0.5 - this.shield.width * 0.5
+	this.heart = new Kiwi.GameObjects.Sprite(
+		this, this.textures.icons, 10, 10 )
+	this.heart.cellIndex = 8
+	this.heart.y = this.game.stage.height - this.heart.height - 10
 
-  this.crown = new Kiwi.GameObjects.Sprite(
-    this, this.textures.icons, 10, 10)
-  this.crown.cellIndex = 10
-  this.crown.x = this.game.stage.width - this.crown.width - 10
-  this.crown.y = this.game.stage.height - this.crown.height - 10
 
-  this.bomb = new Kiwi.GameObjects.Sprite(
-    this, this.textures.icons, 0, 10)
-  this.bomb.x = this.game.stage.width - this.bomb.width - 10
+	this.shield = new Kiwi.GameObjects.Sprite(
+		this, this.textures.icons, 200, 200 )
+	this.shield.cellIndex = 9
+	this.shield.y = this.game.stage.height * 0.5 - this.shield.height * 0.5
+	this.shield.x = this.game.stage.width * 0.5 - this.shield.width * 0.5
 
-  // Add the GameObjects to the stage
-  this.addChild(this.heart)
-  this.addChild(this.crown)
-  this.addChild(this.shield)
-  this.addChild(this.bomb)
-  this.addChild(this.name)
+
+	this.crown = new Kiwi.GameObjects.Sprite(
+		this, this.textures.icons, 10, 10 )
+	this.crown.cellIndex = 10
+	this.crown.x = this.game.stage.width - this.crown.width - 10
+	this.crown.y = this.game.stage.height - this.crown.height - 10
+
+
+	this.bomb = new Kiwi.GameObjects.Sprite(
+		this, this.textures.icons, 0, 10 )
+	this.bomb.x = this.game.stage.width - this.bomb.width  -10
+
+
+	// Add the GameObjects to the stage
+	this.addChild( this.heart )
+	this.addChild( this.crown )
+	this.addChild( this.shield )
+	this.addChild( this.bomb )
+	this.addChild( this.name )
 }
 
-TemplateGame.Play.update = function () {
-  Kiwi.State.prototype.update.call(this)
-
-  this.shield.rotation += this.game.time.clock.rate * 0.01
+var point
+TemplateGame.Play.update = function() {
+point =  new Kiwi.Geom.Point( this.game.input.x, this.game.input.y )
+shieldPoint = new Kiwi.Geom.Point( this.shield.x+this.shield.width/2 , this.shield.y +this.shield.height/2 )
+	Kiwi.State.prototype.update.call( this )
+	this.shield.rotation =  shieldPoint.angleTo( point ) +90
 }
