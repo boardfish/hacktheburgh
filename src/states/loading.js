643b5ec0260088d7371b10a9572dee2de7b67d7c
Greg Ives <gregives774@gmail.com>
2018-03-10 14:09:54 +0000

    Format code to standardjs

--- src/states/loading.js
+++ src/states/loading.js
@@ -16,33 +16,31 @@
 * with some custom code to handle the loading/bobbing/fading of all the items,
 * so if you override a method (e.g. the preload) make sure you call the
 * super method.
-* 
+*
 * The parameters we are passing into this method are as follows:
 * 1 - name {string} Name of this state.
 * 2 - stateToSwitch {string} Name of the state to switch to AFTER all the
-*	assets have loaded. Note: The state you want to switch to should already
-*	have been added to the game.
+* assets have loaded. Note: The state you want to switch to should already
+* have been added to the game.
 * 3 - dimensions {object} A Object containing the width/height that the game
-*	is to be. For example `{width: 1024, height: 768}`
+* is to be. For example `{width: 1024, height: 768}`
 * 4 - subfolder {string} Folder that the loading graphics are located in
 */
 
-var TemplateGame = TemplateGame || {};
+var TemplateGame = TemplateGame || {}
 
 TemplateGame.Loading = new KiwiLoadingScreen(
-	"Loading", "Intro", "assets/img/loading/" );
-
+  'Loading', 'Intro', 'assets/img/loading/')
 
 TemplateGame.Loading.preload = function () {
+  // Make sure to call the super at the top.
+  // Otherwise the loading graphics will load last,
+  // and that defies the whole point in loading them.
+  KiwiLoadingScreen.prototype.preload.call(this)
 
-	// Make sure to call the super at the top.
-	// Otherwise the loading graphics will load last,
-	// and that defies the whole point in loading them.
-	KiwiLoadingScreen.prototype.preload.call(this);
-
-	/**
-	* Replace with your own in-game assets to load.
-	**/
-	this.addImage( "kiwiName", "assets/img/kiwijs-name.png" );
-	this.addSpriteSheet( "icons", "assets/img/kiwijs-icons.png", 100, 90 );
-};
+  /**
+  * Replace with your own in-game assets to load.
+  **/
+  this.addImage('kiwiName', 'assets/img/kiwijs-name.png')
+  this.addSpriteSheet('icons', 'assets/img/kiwijs-icons.png', 100, 90)
+}
