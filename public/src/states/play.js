306f28bacb8d08a7db7b4319543365809fab0c81
Greg Ives <gregives774@gmail.com>
2018-03-11 02:21:01 +0000

    Added express server with socket.io, change character sprite, add NPCs

--- public/src/states/play.js
+++ public/src/states/play.js
@@ -15,19 +15,29 @@ TemplateGame.Play = new Kiwi.State('Play')
 TemplateGame.Play.create = function () {
   Kiwi.State.prototype.create.call(this)
   this.SHOT_DELAY = 100 // milliseconds (10 balls/second)
-  this.BALL_SPEED = 150 // pixels/second
+  this.BALL_SPEED = 50 // pixels/second
   this.NUMBER_OF_BALLS = 1
-  // this.game.stage.createDebugCanvas()
+  this.game.stage.createDebugCanvas()
 
-  this.player = new Kiwi.GameObjects.Sprite(this, this.textures.icons, 200, 200)
-  this.player.cellIndex = 9
+  this.player = new Kiwi.GameObjects.Sprite(this, this.textures.player, 36, 36)
+  this.player.animation.add('run', [2, 6, 10, 14], 0.1, true, false)
   this.player.y = this.game.stage.height * 0.5 - this.player.height * 0.5
   this.player.x = this.game.stage.width * 0.5 - this.player.width * 0.5
-  this.npc = new Kiwi.GameObjects.Sprite(this, this.textures.icons, 200, 200)
-  this.npc.cellIndex = 0
-  this.npc.y = (this.game.stage.height * 0.5 - this.npc.height * 0.5) + 100
-  this.npc.x = (this.game.stage.width * 0.5 - this.npc.width * 0.5) + 100
-  this.addChild(this.npc)
+  this.addChild(this.player)
+  this.player.animation.play('run')
+
+  const NUMBER_OF_PLAYERS = 5
+  this.playerPool = new Kiwi.Group(this)
+  this.addChild(this.playerPool)
+  for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
+    var npc = new Kiwi.GameObjects.Sprite(this, this.textures.enemy, 36, 36)
+    npc.animation.add('run', [2, 6, 10, 14], 0.1, true, false)
+    npc.y = this.game.stage.height * Math.random()
+    npc.x = this.game.stage.width * Math.random()
+    this.playerPool.addChild(npc)
+    npc.animation.play('run')
+  }
+
   // Set the pivot point to the center of the player
   this.player.anchorPointX = this.player.width * 0.5
   this.player.anchorPointY = this.player.height * 0.5
@@ -36,12 +46,12 @@ TemplateGame.Play.create = function () {
   this.addChild(this.ballPool)
   for (var i = 0; i < this.NUMBER_OF_BALLS; i++) {
     // Create each ball and add it to the group.
-    var ball = new Kiwi.GameObjects.Sprite(this, this.textures.circle, 40, 40)
+    var ball = new Kiwi.GameObjects.Sprite(this, this.textures.circle, 15, 15)
     this.ballPool.addChild(ball)
 
     // Set the pivot point to the center of the ball
-    ball.anchorPointX = this.player.width * 0.5
-    ball.anchorPointY = this.player.height * 0.5
+    ball.anchorPointX = ball.width * 0.5
+    ball.anchorPointY = ball.height * 0.5
 
     // Enable physics on the ball
     ball.physics = ball.components.add(new Kiwi.Components.ArcadePhysics(ball, ball.box))
@@ -49,7 +59,6 @@ TemplateGame.Play.create = function () {
     // Set its initial state to "dead".
     ball.alive = false
   }
-  this.addChild(this.player)
 
   this.step = 3
 
@@ -61,38 +70,36 @@ TemplateGame.Play.create = function () {
 }
 
 TemplateGame.Play.shootBall = function () {
-    // Enforce a short delay between shots by recording
-    // the time that each ball is shot and testing if
-    // the amount of time since the last shot is more than
-    // the required delay.
+  // Enforce a short delay between shots by recording
+  // the time that each ball is shot and testing if
+  // the amount of time since the last shot is more than
+  // the required delay.
   if (this.lastBallShotAt === undefined) this.lastBallShotAt = 0
   if (this.game.time.now() - this.lastBallShotAt < this.SHOT_DELAY) return
   this.lastBallShotAt = this.game.time.now()
 
-    // Get a dead ball from the pool
+  // Get a dead ball from the pool
   var ball = this.getFirstBall(false)
 
-    // If there aren't any balls available then don't shoot
+  // If there aren't any balls available then don't shoot
   if (ball === null || ball === undefined) return
 
-    // Revive the ball
-    // This makes the ball "alive"
+  // Revive the ball
+  // This makes the ball "alive"
   this.revive(ball)
 
-    // Set the ball position to the player position.
+  // Set the ball position to the player position.
   var ballOffsetX = ball.width * 0.5
   var ballOffsetY = ball.height * 0.5
   var playerOffsetX = this.player.width * 0.5
   var playerOffsetY = this.player.height * 0.5
-  this.centerPoint = new Kiwi.Geom.Point(
-    this.player.x + playerOffsetX,
-    this.player.y + playerOffsetY )
+  this.centerPoint = new Kiwi.Geom.Point(this.player.x + playerOffsetX, this.player.y + playerOffsetY)
   ball.x = this.centerPoint.x - ballOffsetX
   ball.y = this.centerPoint.y - ballOffsetY
 
-  ball.rotation = this.player.rotation
+  ball.rotation = this.player.rotation - Math.PI / 2
 
-    // Shoot it in the right direction
+  // Shoot it in the right direction
   ball.physics.velocity.x = Math.cos(ball.rotation) * this.BALL_SPEED
   ball.physics.velocity.y = Math.sin(ball.rotation) * this.BALL_SPEED
 }
@@ -104,18 +111,27 @@ TemplateGame.Play.getFirstBall = function (alive) {
     if (balls[i].alive === alive) {
       return balls[i]
     }
-  };
+  }
   return null
 }
 
 TemplateGame.Play.revive = function (ball) {
   ball.alive = true
 }
+
 TemplateGame.Play.checkBallPosition = function (ball) {
   if (ball.x > this.game.stage.width || ball.x < 0 ||
-        ball.y > this.game.stage.height || ball.y < 0) {
+      ball.y > this.game.stage.height || ball.y < 0) {
     ball.alive = false
   }
+
+  var oldVelX = ball.physics.velocity.x
+  var newVelX = oldVelX - Math.cos(ball.rotation)
+  ball.physics.velocity.x = newVelX <= 1 && newVelX >= -1 ? 0 : newVelX
+
+  var oldVelY = ball.physics.velocity.y
+  var newVelY = oldVelY - Math.sin(ball.rotation)
+  ball.physics.velocity.y = newVelY <= 1 && newVelY >= -1 ? 0 : newVelY
 }
 
 TemplateGame.Play.angleToPointer = function () {
@@ -124,9 +140,7 @@ TemplateGame.Play.angleToPointer = function () {
   this.mousePoint = new Kiwi.Geom.Point(this.mouse.x, this.mouse.y)
   this.centerPoint = new Kiwi.Geom.Point(
     this.player.x + this.game.cameras.defaultCamera.transform.x + playerOffsetX,
-    this.player.y + this.game.cameras.defaultCamera.transform.y + playerOffsetY, )
-  console.log(this.mousePoint.x, this.centerPoint.x)
-  console.log(this.mousePoint.y, this.centerPoint.y)
+    this.player.y + this.game.cameras.defaultCamera.transform.y + playerOffsetY)
   return this.centerPoint.angleTo(this.mousePoint)
 }
 
@@ -134,7 +148,7 @@ TemplateGame.Play.update = function () {
   Kiwi.State.prototype.update.call(this)
 
   // Debug - clear canvas from last frame.
-  // this.game.stage.clearDebugCanvas()
+  this.game.stage.clearDebugCanvas()
 
   // Move the player with the arrow keys.
   if (this.leftKey.isDown) {
@@ -149,7 +163,7 @@ TemplateGame.Play.update = function () {
   if (this.downKey.isDown) {
     this.player.y += this.step
   }
-  this.player.rotation = this.angleToPointer()
+  this.player.rotation = this.angleToPointer() + Math.PI / 2
 
   if (this.game.input.mouse.isDown) {
     this.shootBall()
@@ -159,14 +173,15 @@ TemplateGame.Play.update = function () {
 
   // Check if player is intersecting with ball.
   var ball = this.getFirstBall(true)
-  if (ball !== null && this.npc.exists) {
-      var overlapped = Kiwi.Geom.Intersect.rectangleToRectangle(ball.box.bounds, this.npc.box.bounds)
-      console.log(overlapped.result)
-      if (overlapped) {
-        this.npc.destroy()
-      } else {
-        console.log(ball.x)
+  if (ball !== null) {
+    var players = this.playerPool.members
+
+    for (var i = players.length - 1; i >= 0; i--) {
+      var overlapped = Kiwi.Geom.Intersect.rectangleToRectangle(ball.box.bounds, players[i].box.bounds)
+      if (overlapped.result) {
+        players[i].destroy()
       }
+    }
   }
 
   var playerOffsetX = this.player.width * 0.5
@@ -177,5 +192,5 @@ TemplateGame.Play.update = function () {
   this.game.cameras.defaultCamera.transform.y = -1 * this.player.y + this.game.stage.height * 0.5 - playerOffsetY
 
   // Debug - draw debug canvas.
-  // this.player.box.draw(this.game.stage.dctx)
+  this.player.box.draw(this.game.stage.dctx)
 }
